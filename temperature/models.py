a323ac05d25fb4968fc8f1b0eee896c22ce1b119
Anton Konyk <antonkonyk.com>
2024-12-12 19:07:09 +0100

    create models.py for app temperature

---
+++ temperature/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, UniqueConstraint
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class DBTemperature (Base):
+    __tablename__ = "temperatures"
+
+    id = Column(Integer, primary_key=True, index=True)
+    city_id = Column(Integer, ForeignKey("cities.id"))
+    date_time = Column(DateTime, nullable=False)
+    temperature = Column(Integer, nullable=False)
+
+    city = relationship("DBCity", back_populates="temperatures")
+
+    __table_args__ = (
+        UniqueConstraint('city_id', 'date_time', name='_city_date_uc'),
+    )
