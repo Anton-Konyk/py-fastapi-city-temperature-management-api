4c25c5698878c6f3769d6e54b6f8df2a47c265b9
Anton Konyk <antonkonyk.com>
2024-12-13 19:38:13 +0100

    create serialization classes for temperature app

---
+++ temperature/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Any
+
+
+class TemperatureBase(BaseModel):
+    city_id: int
+    date_time: datetime
+    temperature: float
+
+
+class TemperatureCreate(TemperatureBase):
+    city: Any
+
+
+class TemperatureUpdateResponse(BaseModel):
+    message: str
+    updated_records: int
+    temperatures: List[TemperatureCreate]
+
+
+class Temperature(TemperatureBase):
+    id: int
+
+    class Config:
+        from_attributes = True
