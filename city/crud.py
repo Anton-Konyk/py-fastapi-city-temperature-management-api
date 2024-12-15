75c9971721df876371fdac092d710c789c162225
Anton Konyk <antonkonyk.com>
2024-12-15 20:05:19 +0100

    remake to async version

--- city/crud.py
+++ city/crud.py
@@ -1,46 +1,51 @@
 from fastapi import HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from city import schemas, models
 
 
-def create_city(db: Session, city: schemas.CityCreate):
-    db_city = models.DBCity(
+async def create_city(db: AsyncSession, city: schemas.CityCreate):
+    query = insert(models.DBCity).values(
         name=city.name,
         additional_info=city.additional_info,
     )
-    db.add(db_city)
-    db.commit()
-    db.refresh(db_city)
+    result = await db.execute(query)
+    await db.commit()
 
-    return db_city
-
-
-def get_city_by_id(db: Session, city_id: int):
-    return (
-        db.query(models.DBCity).filter(models.DBCity.id == city_id).first()
-    )
+    resp = {**city.model_dump(), "id": result.lastrowid}
+    return resp
 
 
-def get_city_by_name_additional_info(
-        db: Session,
+async def get_city_by_id(db: AsyncSession, city_id: int):
+    query = select(models.DBCity).where(models.DBCity.id == city_id)
+    result = await db.execute(query)
+    return result.scalars().first()
+
+
+async def get_city_by_name_additional_info(
+        db: AsyncSession,
         city_name: str,
         city_additional_info: str
 ):
-    return (
-        db.query(models.DBCity).
-        filter((models.DBCity.name == city_name) &
-               (models.DBCity.additional_info == city_additional_info)).
-        first()
+    query = select(models.DBCity).where((models.DBCity.name == city_name) &
+                                        (models.DBCity.additional_info ==
+                                         city_additional_info))
+    result = await db.execute(query)
+    return result.scalars().first()
+
+
+async def get_cities(db: AsyncSession, skip: int = 0, limit: int | None = 10):
+    query = await db.execute(
+        select(models.DBCity).offset(skip).limit(limit)
     )
+    return query.scalars().all()
 
 
-def get_cities(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.DBCity).offset(skip).limit(limit).all()
-
-
-def update_city(db: Session, city_id: int, city: schemas.CityUpdate):
-    db_city = db.query(models.DBCity).filter(models.DBCity.id == city_id).first()
+async def update_city(db: AsyncSession, city_id: int, city: schemas.CityUpdate):
+    query = select(models.DBCity).where(models.DBCity.id == city_id)
+    result = await db.execute(query)
+    db_city = result.scalars().first()
 
     if not db_city:
         raise HTTPException(status_code=404, detail="City not found")
@@ -48,7 +53,7 @@ def update_city(db: Session, city_id: int, city: schemas.CityUpdate):
     db_city.name = city.name
     db_city.additional_info = city.additional_info
 
-    db.commit()
-    db.refresh(db_city)
+    await db.commit()
+    await db.refresh(db_city)
 
     return db_city
