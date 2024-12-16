9f7e0c6669a2da7f256cd7af9d48ed999183ccdf
Anton Konyk <antonkonyk.com>
2024-12-16 13:43:47 +0100

    apply .env settings

--- settings.py
+++ settings.py
@@ -1,9 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic.v1 import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "City temperature management FastAPI"
-    DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_temperature.db"
+    DATABASE_URL: str | None = os.getenv("DATABASE_URL")
+    DEBUG: bool = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
     class Config:
         case_sensitive = True
